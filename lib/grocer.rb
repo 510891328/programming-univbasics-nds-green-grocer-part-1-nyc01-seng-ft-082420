d934850fceddfbff6025cfdff85bb7d1fd79b539
Yunhao Gao <as510891328@gmail.com>
2020-08-19 23:26:47 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,9 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
-  
+  collection.each { |k, v|
+    if k
+  }
 end
 
 def consolidate_cart(cart)
