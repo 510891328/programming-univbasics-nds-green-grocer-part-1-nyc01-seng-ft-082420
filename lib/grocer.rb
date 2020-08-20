1d13292d2e904b3b169d47294c97ee071efb0a45
Yunhao Gao <as510891328@gmail.com>
2020-08-20 02:06:35 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -28,7 +28,7 @@ def consolidate_cart(cart)
   cart.each { |v, i|
    cart_item[v[:item]][:price] = v[:price]
    cart_item[v[:item]][:clearance] = v[:clearance]
-   if cart_item[v[:item]][:count]
+   if cart_item[v[:item]][:count] 
   }
   
 
