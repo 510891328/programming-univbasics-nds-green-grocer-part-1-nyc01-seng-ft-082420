17075a47bb99f7f0af4eb5d9ba2fe7c6f6f3cf2f
Yunhao Gao <as510891328@gmail.com>
2020-08-20 00:08:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,12 @@
+require "pry"
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
   item = {}
   collection.each { |v, i|
+  binding.pry
     if v[:item] == name
       item = collection[i]
     end
